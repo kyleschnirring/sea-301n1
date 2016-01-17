3149bbc543af22ad6e89c4fb46d19277f07f06e6
kyleschnirring <kyleschnirring@icloud.com>
2016-01-17 11:38:22 -0800

    finished with the html

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -13,20 +13,19 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-  var context = {
-    title: "My New Post",
-    body: "This is my first post!"
-  };
 
+  var template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+
+  return template(this);
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
 };
 
